d0b1eca3083e0c7bb94d9e7ff543760dadf7eba9
fabianokeke <okekefabian123@gmail.com>
2020-03-29 22:20:28 +0200

    Add three files

---
+++ src/try_django/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home_page(request):
+    my_title = 'Hello there ....'
+    return render(request, "hello_world.html", {'title': my_title})
+
+
+def about_page(request):
+    return render(request, "about.html", {'title': 'About'})
+
+
+def contact_page(request):
+    return render(request, "hello_world.html", {'title': 'call yourself'})
